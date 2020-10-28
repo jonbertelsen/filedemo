bba74db032f1a1a29a9ceb5f188644aacfdbd03f
Jon Bertelsen <jobe@cphbusiness.dk>
2020-10-28 14:01:57 +0100

    Added menu system

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,83 @@
+import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
-
+        menu();
+    }
+    public static void menu() {
         Imdb imdb = new Imdb();
-
-        //imdb.addMovie(new Movie("First blood 1", 7.7, 1982));
-        //imdb.addMovie(new Movie("Terminator 1", 8.0, 1984));
-        //imdb.addMovie(new Movie("Fjern mig", 8.0, 1984));
-        //imdb.addMovie(new Movie("First blood 1", 7.7, 1982));
-
-        //imdb.showMovies();
-        //System.out.println("Nu har vi fjernet en film:");
-        //imdb.removeMovie("First blood 1");
-
         imdb.readMoviesFromFile("imdbfiles.txt");
-
+        boolean running = true;
+        while (running) {
+            showMenu();
+            int choose = getInt("What do you wanna do? : ");
+            switch (choose) {
+                case 1:
+                    showMovieList(imdb);
+                    break;
+                case 2:
+                    addMovie(imdb);
+                    break;
+                case 3:
+                    removeMovie(imdb);
+                    break;
+                case 4:
+                    running = false;
+                    break;
+            }
+        }
+        System.out.println("Goodbye");
+    }
+    private static void showMenu() {
+        System.out.println(" ────────────────────────────────────────────────────────────────");
+        System.out.println(" │ " + "[1] Movie list" + " │ " + "[2] Add movie" + " │ " + "[3] Remove movie" + " │ " + "[4] Exit" + " │ ");
+        System.out.println(" ────────────────────────────────────────────────────────────────");
+    }
+    private static void showMovieList(Imdb imdb) {
+        System.out.println("\n" + "Movie list :");
         imdb.showMovies();
-        //imdb.saveMoviesToFile("imdbfiles.txt");
-
+        System.out.println("\n");
+    }
+    private static void addMovie(Imdb imdb) {
+        String title = getString("Title : ");
+        imdb.addMovie(new Movie(title, getDouble("Rating : "), getInt("release year : ")));
+        System.out.println(title + " has been added");
+        imdb.saveMoviesToFile("imdbfiles.txt");
+    }
+    private static void removeMovie(Imdb imdb) {
+        String title = (getString("Which movie do you wish to remove? : "));
+        imdb.removeMovie(title);
+        System.out.println(title + " has been removed");
+        imdb.saveMoviesToFile("imdbfiles.txt");
+    }
+    public static String getString(String s) {
+        System.out.print(s);
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+    public static int getInt(String question) {
+        int i;
+        while (true) {
+            try {
+                String answer = getString(question);
+                i = Integer.parseInt(answer);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("input ikke gyldigt");
+            }
+        }
+        return i;
+    }
+    public static double getDouble(String question) {
+        double i;
+        while (true) {
+            try {
+                String answer = getString(question);
+                i = Double.parseDouble(answer);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Input ikke gyldigt");
+            }
+        }
+        return i;
     }
 }
